ea13428c91121d02d6a1398e426aa8360ee1a9b8
AmitaiB <blickstein@gmail.com>
2017-12-13 11:33:39 -0500

    p.42 - Wiring Up TableViewCell

---
+++ SportsStore/TableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  TableViewCell.swift
+//  SportsStore
+//
+//  Created by Amitai Blickstein on 12/13/17.
+//  Copyright © 2017 Amitai Blickstein. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var stockStepper: UIStepper!
+    @IBOutlet weak var stockField: UITextField!
+}

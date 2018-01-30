35b830915f43db969d7bb7588d5246b73ae524cf
AmitaiB <blickstein@gmail.com>
2018-01-30 13:14:45 -0500

    p. 203ff - Adds SalesTaxRate, and 2 subclasses of Product
    Setting up for a Factory Method

--- SportsStore/Product.swift
+++ SportsStore/Product.swift
@@ -14,6 +14,7 @@ class Product: NSObject, NSCopying {
     private(set) var category: String
     private var stockBackingValue = 0
     private var priceBackingValue = 0.0
+    fileprivate var salesTaxRate  = 0.2
     
     var stockLevel: Int {
         get { return stockBackingValue }
@@ -26,11 +27,17 @@ class Product: NSObject, NSCopying {
     }
     
     var stockValue: Double {
-        return price * Double(stockLevel)
+        let taxedPrice = price * (1 + salesTaxRate)
+        return taxedPrice * Double(stockLevel)
     }
     
-    
-    init(name aName: String, description aDescription: String, category aCategory: String, price aPrice: Double, stockLevel aStockVal: Int) {
+
+    init(name aName: String,
+         description aDescription: String,
+         category aCategory: String,
+         price aPrice: Double,
+         stockLevel aStockVal: Int)
+    {
         name = aName
         productDescription = aDescription
         category = aCategory
@@ -50,4 +57,45 @@ class Product: NSObject, NSCopying {
     func copy(with zone: NSZone? = nil) -> Any {
         return Product(name: name, description: productDescription, category: category, price: price, stockLevel: stockLevel)
     }
+    
+    var upsells: [UpsellOpportunities] {
+        get { return Array() }
+    }
 }
+
+enum UpsellOpportunities {
+    case swimmingLessons
+    case mapOfLakes
+    case soccerVideos
+}
+
+class WatersportsProduct: Product {
+    required override init(name aName: String, description aDescription: String, category aCategory: String, price aPrice: Double, stockLevel aStockVal: Int)
+    {
+        super.init(name: aName, description: aDescription, category: aCategory, price: aPrice, stockLevel: aStockVal)
+        
+        salesTaxRate = 0.10
+    }
+    
+    override var upsells: [UpsellOpportunities] {
+        return [.swimmingLessons, .mapOfLakes]
+    }
+}
+
+class SoccerProduct: Product {
+        required override init(name aName: String, description aDescription: String, category aCategory: String, price aPrice: Double, stockLevel aStockVal: Int)
+        {
+            super.init(name: aName, description: aDescription, category: aCategory, price: aPrice, stockLevel: aStockVal)
+            
+            salesTaxRate = 0.25
+        }
+        
+        override var upsells: [UpsellOpportunities] {
+            return [.soccerVideos]
+        }
+}
+
+
+
+
+
